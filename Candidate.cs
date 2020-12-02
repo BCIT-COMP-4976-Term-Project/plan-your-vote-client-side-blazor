05c297d46c75def8d591f4586e958b14bc3321af
John Poku <jvbpoku@hotmail.com>
2020-12-02 01:12:39 -0800

    sorry moving to new feature branch right now

---
+++ Candidate.cs
@@ -0,0 +1,12 @@
+    class ElectionCandidate
+    {
+        string Canadidateid { get; set; }
+        string ElectionId { get; set; }
+        string Election { get; set; } 
+        string Name { get; set; } 
+        string Picture { get; set; }
+        string Details { get; set; }
+        string OrganizationId { get; set; }
+        string CanadidateRaces { get; set; }
+        string Contacts { get; set; }
+    }
